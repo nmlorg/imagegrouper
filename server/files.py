1ea3fbdf16e3639702970c5c4475f485bebff321
Daniel Reed <nmlorg@gmail.com>
2023-06-05 19:12:17 -0700

    Slightly simplify things.
    Just show all images under the current directory, don't force users to use a specific directory structure.
    
    Each subdirectory is a separate group.

--- server/files.py
+++ server/files.py
@@ -2,10 +2,10 @@ import os
 
 
 def _find():
-    for path, subdirs, files in os.walk('images'):
+    for path, subdirs, files in os.walk('.'):
         for fname in files:
             if fname.lower().rsplit('.', 1)[-1] in ('jpeg', 'jpg', 'png'):
-                yield os.path.join(path, fname)
+                yield os.path.join(path, fname)[len('./'):]
 
 
 FILES = list(_find())
